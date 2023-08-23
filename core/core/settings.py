02c98e99beed4ca6f0f7c6b6373d66e2313298d0
hamid <hamidrezaowji@gmail.com>
2023-08-23 18:54:00 +0200

    set up for user account completed

--- core/core/settings.py
+++ core/core/settings.py
@@ -158,13 +158,13 @@ SIMPLE_JWT = {
     # other JWT settings
 }
 
-# CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = True
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:5173",
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
 
 
 
